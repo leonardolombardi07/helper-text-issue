e6bb5ca2381a2d4a8bb8d8b585992acb58c97d43
Leonardo <leonardolombardi@poli.ufrj.br>
2022-01-08 11:46:00 +0100

    First commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { Provider as PaperProvider } from "react-native-paper";
+import { StyleSheet, Text, View } from "react-native";
+import LoggedInScreen from "./src/screens/LoggedInScreen";
 
-export default function App() {
+export default function AppWithProviders() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <App />
+    </PaperProvider>
   );
+}
+
+function App() {
+  return <LoggedInScreen />;
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
